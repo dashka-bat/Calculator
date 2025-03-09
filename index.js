f782d87c03b99db5476937ad4a7a438705601727
dashka-bat <dushkab02@gmail.com>
2025-03-10 01:51:06 +0800

    end

---
+++ index.js
@@ -0,0 +1,13 @@
+const display=document.getElementById('display');
+function appendToDisplay(num){
+    display.value+=num;
+}
+function clearDisplay(){
+    display.value='';
+}
+function calculate(){  
+    try{
+          display.value=eval(display
+    .value);                }   catch(err){display.value='Error';}    
+    }
+  
